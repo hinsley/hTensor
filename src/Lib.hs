d338a9da66dfcf3b3fa7f76f42124108f562a989
Carter Hinsley <carterhinsley@outlook.com>
2016-05-27 00:11:35 -0400

    Add CPython interfacing

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import System.IO (stdout)
+import qualified CPython as Py
+import qualified CPython.Protocols.Object as Py
+import qualified CPython.Types as Py
+import qualified CPython.Types.Module as Py
+import qualified Data.Text as T
+
+pyLoadTF = do
+    Py.initialize
+    tf <- Py.importModule . T.pack $ "tensorflow"
+    return tf
+
+pyPrint :: String -> IO ()
+pyPrint s = do
+    Py.initialize
+    pyString <- Py.toUnicode . T.pack $ s
+    Py.print pyString stdout
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
